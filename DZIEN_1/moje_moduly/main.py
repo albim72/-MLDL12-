f146d8a8a2d5e29c77571cd0a4abebbea5511041
albim72 <marcin.albiniak72@gmail.com>
2024-12-16 10:31:16 +0100

    Create main.py

---
+++ DZIEN_1/moje_moduly/main.py
@@ -0,0 +1,9 @@
+# import dane
+# import dane as dn
+from dane import nr_filii as nf
+from dane import book as bk
+
+print("wykorzystanie modułów języka Python")
+print("_______ kolekcje -> dane __________")
+print(nf)
+print(bk)
