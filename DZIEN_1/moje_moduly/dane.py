7cb3f454994c5e906cfbd5e50d82315ee634fc77
albim72 <marcin.albiniak72@gmail.com>
2024-12-16 10:26:00 +0100

    Create dane.py

---
+++ DZIEN_1/moje_moduly/dane.py
@@ -0,0 +1,9 @@
+nr_filii = [2,8,12,34,78,54]
+
+book = {
+        "tytuł":"ABC Kulturysty",
+        "autor":"Janusz Kowalski",
+        "cena":67,
+        "liczba stron":412,
+        "numer filii":nr_filii
+}
