947b9159020c8220dc8138fd5b62ac5853b99214
albim72 <marcin.albiniak72@gmail.com>
2024-12-16 11:10:27 +0100

    Update main.py

--- DZIEN_1/moje_moduly/main.py
+++ DZIEN_1/moje_moduly/main.py
@@ -2,8 +2,13 @@
 # import dane as dn
 from dane import nr_filii as nf
 from dane import book as bk
+from mojefunkcje.collectfunction import czytaj_liste, czytaj_slownik
 
 print("wykorzystanie modułów języka Python")
 print("_______ kolekcje -> dane __________")
 print(nf)
 print(bk)
+
+print("________ użycie pakietu mojefunkcje ____________")
+czytaj_liste(nf)
+czytaj_slownik(bk)
